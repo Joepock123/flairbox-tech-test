50c1ff5ae1c8009c9a30ec2d6ed5f405c4c0fb00
Joe Jackson <joe.m.jackson@live.co.uk>
2020-07-24 09:39:00 +0100

    Add form elements and build handleChange function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [currentInputs, setCurrentInputs] = useState({
+    sentence: "",
+    option1: "",
+    option2: "",
+    option3: "",
+    option4: "",
+  });
+
+  const handleChange = (e) => {
+    const property = e.target.name;
+    const value = e.target.value;
+    const newState = { ...currentInputs, [property]: value };
+    console.log("App -> currentInputs", currentInputs);
+    setCurrentInputs(newState);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="sentence">Main Sentence</label>
+          <input
+            type="text"
+            id="sentence"
+            name="sentence"
+            onChange={handleChange}
+            required
+          />
+          <input type="submit" />
+        </form>
+
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="option1">option1</label>
+          <input
+            type="text"
+            id="option1"
+            name="option1"
+            onChange={handleChange}
+            required
+          />
+          <input type="submit" />
+        </form>
+
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="option2">option2</label>
+          <input
+            type="text"
+            id="option2"
+            name="option2"
+            onChange={handleChange}
+            required
+          />
+          <input type="submit" />
+        </form>
+
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="option3">option3</label>
+          <input
+            type="text"
+            id="option3"
+            name="option3"
+            onChange={handleChange}
+            required
+          />
+          <input type="submit" />
+        </form>
+
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="option4">option4</label>
+          <input
+            type="text"
+            id="option4"
+            name="option4"
+            onChange={handleChange}
+            required
+          />
+          <input type="submit" />
+        </form>
+      </div>
+    </>
   );
 }
 
